f053fac98da9912128a4d515512c7d8fcbdd5ea2
Ahmadre <rebar.ahmad@telexiom.de>
2020-09-30 03:03:45 +0200

    Add PWA-Support | Add Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBC2PDe9882Zg8jvkTc7JaDVt3Jv53B_bQ',
+    authDomain: 'ngtelexiom.firebaseapp.com',
+    databaseURL: 'https://ngtelexiom.firebaseio.com',
+    projectId: 'ngtelexiom',
+    storageBucket: 'ngtelexiom.appspot.com',
+    messagingSenderId: '244904055676',
+    appId: '1:244904055676:web:5d7f24639cd69978fb0af9'
+  }
 };
 
 /*
